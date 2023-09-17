5c549a2d7dd79c1195b5924874b89c72b1081892
andrea-canestrari <132762176+andrea-canestrari@users.noreply.github.com>
2023-09-17 13:24:32 +0200

    Aggiornamento file di prova (test_fat.c)

--- test_fat.c
+++ test_fat.c
@@ -1,15 +1,52 @@
 #include "fat.h"
 
 int main(){
-    char read_buff[10];
+    char read_buff[10] = {0};
     virtual_disk* vd = startFAT("provaFileSystemFat");
     printf("startFAT completed!\n");
     printf("\n");
     printf("\n");
-    listDir(vd);
-    FileHandle* file = createFile(vd, "fileditesto.txt");
+    FileHandle* file = createFile(vd, "fileditesto.txt", 'x');
     printf("createFile completed!\n");
     printf("\n");
+    printf("\n");
+    FileHandle* file2 = createFile(vd, "filediprova.txt", 'x');
+    printf("createFile completed!\n");
+    printf("\n");
+    printf("\n");
+
+    FileHandle* file3 = createFile(vd, "file.txt", 'x');
+    printf("createFile completed!\n");
+    printf("\n");
+    printf("\n");
+
+    int res= createDir(vd, "dirDiProva1");   
+    printf("createDir completed! Esito == %d\n", res);
+    printf("\n");
+    printf("\n");
+
+    res= createDir(vd, "dirDiProva2");   
+    printf("createDir completed! Esito == %d\n", res);
+    printf("\n");
+    printf("\n");
+
+
+    for (int i=0; i< MAX_BLOCKS; i++){
+       printf("FAT table pos n.%d == %d\n", i, vd->disk->f_table[i]);
+    }
+
+    for (int i=0; i< MAX_ENTRIES; i++){
+       printf("dir table pos n.%d == %d\n", i, vd->disk->d_table[i].first_fat_block);
+       printf("%s\n", vd->disk->d_table[i].name);
+    }
+
+    res = eraseDir(vd, "dirDiProva2");
+    printf("eraseDir completed! Esito == %d\n", res);
+    printf("\n");
+    printf("\n");
+
+
+    dir_array* test=listDir(vd);
     printf("\n");
     int written = FAT_write(file, "tottiGol", sizeof("tottiGol"));
     printf("\n");
@@ -19,41 +56,49 @@ int main(){
     printf("\n");
     printf("\n");
     printf("FAT_write completed! Caratteri scritti == %d\n", written);
-    
-    
     int ris = FAT_seek(file, 1, SEEK_SET);
     printf("\n");
     printf("\n");
-    printf("FAT_seek completed! Esito == %d\n", ris);
+    printf("FAT_seek completed! Esito == %d, nuova posizione == %d\n", ris, file->pos);
     printf("\n");
     int read = FAT_read(file, read_buff, sizeof(read_buff));
     printf("\n");
     printf("\n");
     printf("FAT_read completed! Caratteri letti == %d\n", read);
     printf("\n");
-    dir_array* test =listDir(vd);
-    int res= createDir(vd, "dirDiProva1");   
-    printf("createDir completed! Esito == %d\n", res);
+    res=eraseFile(vd, "fileditesto.txt");
+    printf("eraseFile completed! Esito ==%d\n", res);
     printf("\n");
     printf("\n");
-    listDir(vd);
+    
     res=changeDir(vd, "dirDiProva1");
     printf("changeDir completed! Esito ==%d\n", res);
     printf("\n");
     printf("\n");
-    res=eraseFile(vd, "file12432.txt");
-    printf("eraseFile completed! Esito ==%d\n", res);
-    printf("\n");
-    printf("\n");
-    res=eraseFile(vd, "mitboll1");
-    printf("eraseFile completed! Esito ==%d\n", res);
-    printf("\n");
-    printf("\n");
     res=changeDir(vd, "/");
     printf("changeDir completed! Esito ==%d\n", res);
+
+    printf("working dir == %d\n", vd->curr_dir);
     printf("\n");
     printf("\n");
-    test=listDir(vd);
+
+    res = eraseFile(vd, "filediprova.txt");
+    printf("eraseFile completed! Esito ==%d\n", res);
+    printf("\n");
+    printf("\n");
+
+    res = eraseFile(vd, "file.txt");
+    printf("eraseFile completed! Esito ==%d\n", res);
+    printf("\n");
+    printf("\n");
+
+    res = eraseDir(vd, "dirDiProva1");
+    printf("eraseDir completed! Esito == %d\n", res);
+    printf("\n");
+    printf("\n");
+
+
+
     printf("\n");
     res=killFAT(vd);
     printf("\n");
